49c27e45e771bd781e5082a23d8307cc23253e98
gbmao <gabriel.machado.oli@gmail.com>
2026-03-02 15:03:04 -0300

    test: create when two cat are created should return different id's

--- src/test/java/com/appdafamilia/service/CatServiceTest.java
+++ src/test/java/com/appdafamilia/service/CatServiceTest.java
@@ -55,15 +55,29 @@ public class CatServiceTest {
         assertEquals(expectedMessage, thrown.getMessage());
     }
 
+    @DisplayName("Check if it will save one time")
     @Test
     void testCreateCat_whenGivenValidInformation_thenSaveOneTime(){
         // Arrange
-
         Mockito.when(catRepository.save(any(Cat.class)))
                 .thenReturn(true);
         // Act
         catService.createCat("Boris");
         // Assert
         Mockito.verify(catRepository, times(1)).save(any(Cat.class));
+    }
+
+    @DisplayName("should return two cats with different ID's")
+    @Test
+    void testCreateCat_whenTwoCatAreCreated_shouldReturnCatsWithDifferentIds() {
+        // Arrange
+        Mockito.when(catRepository.save(any(Cat.class)))
+                .thenReturn(true);
+        // Act
+        CatDto cat1 = catService.createCat("Boris");
+        CatDto cat2 = catService.createCat("Samanta");
+        // Assert
+        assertNotEquals(cat1.id() ,cat2.id());
+
     }
 }
